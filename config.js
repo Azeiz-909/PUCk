d5fa4c116d9c812785c29adcc3393d47fd347e6b
Azeiz-909 <harbigoat3@gmail.com>
2026-07-22 00:48:14 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ضع بيانات مشروع Supabase الخاص بك هنا
+// تجدها في: Supabase Dashboard -> Project Settings -> API
+
+window.SUPABASE_URL = "https://swuticfptxbhkuphkecr.supabase.co";       // مثال: https://xxxxxxxx.supabase.co
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3dXRpY2ZwdHhiaGt1cGhrZWNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2NTk3ODYsImV4cCI6MjEwMDIzNTc4Nn0.tS8he4t8BHpJyFrnNxW8noRnlyrlks5rZZEg0KFnMzQ"; // مفتاح anon public (وليس service_role)
